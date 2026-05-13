7951700942e0dbfd9bdf571e410526818d6ffbf2
Stanley Wei <stanley.wei.04@gmail.com>
2026-05-13 11:32:44 -0700

    Add cascaded controller + significant controller rewrite

--- src/gnc/bur_controller/include/bur_controller/ControllerNode.hpp
+++ src/gnc/bur_controller/include/bur_controller/ControllerNode.hpp
@@ -8,7 +8,6 @@
 #include "bur_msgs/msg/command.hpp"
 #include <geometry_msgs/msg/wrench_stamped.hpp>
 #include <geometry_msgs/msg/pose.hpp>
-#include <control_toolbox/pid.hpp>
 #include <algorithm>
 #include "utility_func.hpp"
 #include <tf2/LinearMath/Quaternion.h>
@@ -17,37 +16,93 @@
 
 namespace controller
 {
-  class Pid{
-  public:
+  class PID {
+  public:    
+    PID (){}
+
+    void setGains(double kp, double ki, double kd)
+    {
+      this->kp = kp;
+      this->kd = kd;
+      this->ki = ki;
+    }
+
+    double computeCommand(double error, double dt)
+    {
+      double currentE = error; // Error is already calculated in the function calls
+      double deriv = (currentE - pastE) / dt;
+      rsum += currentE * dt;
+      double o = kp * currentE + kd * deriv + rsum * ki;
+      pastE = currentE;
+      return o;
+    }
+  
+  private:
     double kp = 0;
     double kd = 0;
     double ki = 0;
     double pastE = 0;
     double currentE = 0;
     double rsum = 0;
-    
-    Pid (){}
-    Pid(double a, double b, double c, double, double, bool){
-      kp = a;
-      kd = b;
-      ki = c;
+  };
+
+  class CascadedPositionVelocityController
+  {
+  public:
+    CascadedPositionVelocityController() {}
+
+    void setPositionGains(double kp, double ki, double kd)
+    {
+      position_controller.setGains(kp, ki, kd);
     }
 
-    void setGains(double a, double b, double c, double, double, bool){
-      kp = a;
-      kd = b;
-      ki = c;
+    void setVelocityGains(double kp, double ki, double kd)
+    {
+      velocity_controller.setGains(kp, ki, kd);
     }
 
-    double computeCommand(double error, double dt){ //error is already calculated in the function calls
-      double t = dt/1e9; // turn dt from nanoseconds -> seconds
-      double currentE = error;
-      double deriv = (currentE - pastE) / t;
-      rsum += currentE * t;
-      double o = kp * currentE + kd * deriv + rsum * ki;
-      pastE = currentE;
-      return o;
+    double computeCommand(double current_pos, double target_pos, double current_vel, double target_vel, double dt)
+    {
+      if (this->using_external_target_vel) {
+        if (abs(target_vel) < 0.1) {
+          target_pos = this->position_setpoint;
+        } else {
+          this->position_setpoint = current_pos;
+        }
+      }
+      double position_error = target_pos - current_pos;
+      if (this->using_angles)
+        position_error = angle_wrap_pi(position_error);
+      double vel_target = position_controller.computeCommand(position_error, dt);
+
+      double velocity_error;
+      if (this->using_external_target_vel && abs(target_vel) >= 0.1) {
+        velocity_error = target_vel - current_vel;
+      } else {
+        velocity_error = vel_target - current_vel;
+      }
+      double out = velocity_controller.computeCommand(velocity_error, dt);
+
+      return out;
     }
+
+    void setUsingExternalVelocityTarget(bool using_external_target_vel) {
+      this->using_external_target_vel = using_external_target_vel;
+    }
+
+    void setUsingAngles(bool using_angles) {
+      this->using_angles = using_angles;
+    }
+
+  private:
+    PID position_controller;
+    PID velocity_controller;
+
+    double position_setpoint = 0.0;
+
+    bool using_external_target_vel = false;
+    bool using_angles = false;
+
   };
 
   class ControllerNode : public rclcpp::Node
@@ -56,52 +111,41 @@ namespace controller
     ControllerNode();
 
   private:
-    // publishers
 
     rclcpp::Publisher<geometry_msgs::msg::WrenchStamped>::SharedPtr pubControlEffort;
     rclcpp::Subscription<bur_msgs::msg::Command>::SharedPtr state_setpoint_sub;
-
-    rclcpp::Time prevTime;
-
-    // implementation
-    Pid linear_x;
-    Pid linear_y;
-    Pid linear_z;
-
-    Pid angular_x;
-    Pid angular_y;
-    Pid angular_z;
-
     rclcpp::TimerBase::SharedPtr pubTimer_;
 
-    // bur_rov_msgs::msg::Command command;
+    //  Callbacks
+    rcl_interfaces::msg::SetParametersResult parametersCallback(const std::vector<rclcpp::Parameter> &parameters);
+    void currentCommandCallback(const bur_msgs::msg::Command::SharedPtr msg);
+
+    rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr param_callback;
+
+    void set_constants();
+    void publishState();
+
+    // Controllers
+    CascadedPositionVelocityController linear_x;
+    CascadedPositionVelocityController linear_y;
+    CascadedPositionVelocityController linear_z;
+
+    CascadedPositionVelocityController angular_x;
+    CascadedPositionVelocityController angular_y;
+    CascadedPositionVelocityController angular_z;
+
     geometry_msgs::msg::Pose pose_state;
     geometry_msgs::msg::Pose pose_setpoint;
     geometry_msgs::msg::Twist twist_state;
     geometry_msgs::msg::Twist twist_setpoint;
 
-    //  callbacks
-    rcl_interfaces::msg::SetParametersResult parametersCallback(
-    const std::vector<rclcpp::Parameter> &parameters);
-    rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr param_callback;
-    void currentCommandCallback(const bur_msgs::msg::Command::SharedPtr msg);
-    void set_constants();
-    void publishState();
-
     rclcpp::Time lastTime;
+
     bool active = false;
-    bool new_params = false;
     bool using_joy = true;
-    bool use_command_target = false;
+
     tf2::Vector3 setpoint_angle;
     tf2::Vector3 state_angle;
-    bool depth_hold = false;
-    bool yaw_hold = false;
-    bool roll_hold = false;
-    bool pitch_hold = false;
-    double yaw_hold_pos;
-    double roll_hold_pos = 0;
-    double pitch_hold_pos = 0;
   };
 }
 
--- src/gnc/bur_controller/src/ControllerNode.cpp
+++ src/gnc/bur_controller/src/ControllerNode.cpp
@@ -7,35 +7,10 @@ namespace controller
   {
 
     this->declare_parameter("sub_topic", "command");
-    this->declare_parameter("target_vel_topic", "/cmd_vel");
     this->declare_parameter("pub_topic", "control_effort");
     this->declare_parameter("publish_rate", 100);
-    this->declare_parameter("use_command_target", false);
     this->declare_parameter("using_joy", true);
     this->declare_parameter("debug", false);
-
-    state_setpoint_sub = this->create_subscription<bur_msgs::msg::Command>(
-        this->get_parameter("sub_topic").as_string(), 1,
-        std::bind(&ControllerNode::currentCommandCallback, this, std::placeholders::_1));
-
-    param_callback = this->add_on_set_parameters_callback(bind(&ControllerNode::parametersCallback, this, std::placeholders::_1));
-    pubControlEffort = this->create_publisher<geometry_msgs::msg::WrenchStamped>(this->get_parameter("pub_topic").as_string(), 1);
-
-    int publish_rate = this->get_parameter("publish_rate").as_int();
-
-    pubTimer_ = this->create_wall_timer(
-        std::chrono::milliseconds(1000 / publish_rate), std::bind(&ControllerNode::publishState, this));
-
-    linear_x = Pid(0.0, 0.0, 0.0, 1.0, -1.0, true);
-    linear_y = Pid(0.0, 0.0, 0.0, 1.0, -1.0, true);
-    linear_z = Pid(0.0, 0.0, 0.0, 1.0, -1.0, true);
-
-    angular_x = Pid(0.0, 0.0, 0.0, 1.0, -1.0, true);
-    angular_y = Pid(0.0, 0.0, 0.0, 1.0, -1.0, true);
-    angular_z = Pid(0.0, 0.0, 0.0, 1.0, -1.0, true);
-
-    this->declare_parameter("max_force", 100.0);
-    this->declare_parameter("max_torque", 60.0);
 
     this->declare_parameter("linear_x/p", 0.0);
     this->declare_parameter("linear_x/i", 0.0);
@@ -61,22 +36,60 @@ namespace controller
     this->declare_parameter("angular_z/i", 0.0);
     this->declare_parameter("angular_z/d", 0.0);
 
+    state_setpoint_sub = this->create_subscription<bur_msgs::msg::Command>(
+        this->get_parameter("sub_topic").as_string(), 1,
+        std::bind(&ControllerNode::currentCommandCallback, this, std::placeholders::_1));
+
+    param_callback = this->add_on_set_parameters_callback(bind(&ControllerNode::parametersCallback, this, std::placeholders::_1));
+    pubControlEffort = this->create_publisher<geometry_msgs::msg::WrenchStamped>(this->get_parameter("pub_topic").as_string(), 1);
+
+    int publish_rate = this->get_parameter("publish_rate").as_int();
+
+    pubTimer_ = this->create_wall_timer(
+        std::chrono::milliseconds(1000 / publish_rate), std::bind(&ControllerNode::publishState, this));
+
+    linear_x = CascadedPositionVelocityController();
+    linear_y = CascadedPositionVelocityController();
+    linear_z = CascadedPositionVelocityController();
+
+    angular_x = CascadedPositionVelocityController();
+    angular_y = CascadedPositionVelocityController();
+    angular_z = CascadedPositionVelocityController();
+
+    linear_x.setUsingExternalVelocityTarget(true);
+    linear_y.setUsingExternalVelocityTarget(true);
+    linear_z.setUsingExternalVelocityTarget(true);
+
+    angular_x.setUsingExternalVelocityTarget(true);
+    angular_y.setUsingExternalVelocityTarget(true);
+    angular_z.setUsingExternalVelocityTarget(true);
+
+    angular_x.setUsingAngles(true);
+    angular_y.setUsingAngles(true);
+    angular_z.setUsingAngles(true);
+
     set_constants();
   }
 
   void ControllerNode::set_constants()
   {
-    double max_force = this->get_parameter("max_force").as_double();
-    double max_torque = this->get_parameter("max_torque").as_double();
     using_joy = this->get_parameter("using_joy").as_bool();
-    linear_x.setGains(this->get_parameter("linear_x/p").as_double(), this->get_parameter("linear_x/i").as_double(), this->get_parameter("linear_x/d").as_double(), max_force, -max_force, true);
-    linear_y.setGains(this->get_parameter("linear_y/p").as_double(), this->get_parameter("linear_y/i").as_double(), this->get_parameter("linear_y/d").as_double(), max_force, -max_force, true);
-    linear_z.setGains(this->get_parameter("linear_z/p").as_double(), this->get_parameter("linear_z/i").as_double(), this->get_parameter("linear_z/d").as_double(), max_force, -max_force, true);
 
-    angular_x.setGains(this->get_parameter("angular_x/p").as_double(), this->get_parameter("angular_x/i").as_double(), this->get_parameter("angular_x/d").as_double(), max_torque, -max_torque, true);
-    angular_y.setGains(this->get_parameter("angular_y/p").as_double(), this->get_parameter("angular_y/i").as_double(), this->get_parameter("angular_y/d").as_double(), max_torque, -max_torque, true);
-    angular_z.setGains(this->get_parameter("angular_z/p").as_double(), this->get_parameter("angular_z/i").as_double(), this->get_parameter("angular_z/d").as_double(), max_torque, -max_torque, true);
-    new_params = false;
+    linear_x.setPositionGains(1.0, 0.0, 0.0);
+    linear_y.setPositionGains(1.0, 0.0, 0.0);
+    linear_z.setPositionGains(1.0, 0.0, 0.0);
+
+    angular_x.setPositionGains(1.0, 0.0, 0.0);
+    angular_y.setPositionGains(1.0, 0.0, 0.0);
+    angular_z.setPositionGains(1.0, 0.0, 0.0);
+
+    linear_x.setVelocityGains(this->get_parameter("linear_x/p").as_double(), this->get_parameter("linear_x/i").as_double(), this->get_parameter("linear_x/d").as_double());
+    linear_y.setVelocityGains(this->get_parameter("linear_y/p").as_double(), this->get_parameter("linear_y/i").as_double(), this->get_parameter("linear_y/d").as_double());
+    linear_z.setVelocityGains(this->get_parameter("linear_z/p").as_double(), this->get_parameter("linear_z/i").as_double(), this->get_parameter("linear_z/d").as_double());
+
+    angular_x.setVelocityGains(this->get_parameter("angular_x/p").as_double(), this->get_parameter("angular_x/i").as_double(), this->get_parameter("angular_x/d").as_double());
+    angular_y.setVelocityGains(this->get_parameter("angular_y/p").as_double(), this->get_parameter("angular_y/i").as_double(), this->get_parameter("angular_y/d").as_double());
+    angular_z.setVelocityGains(this->get_parameter("angular_z/p").as_double(), this->get_parameter("angular_z/i").as_double(), this->get_parameter("angular_z/d").as_double());
   }
 
   rcl_interfaces::msg::SetParametersResult ControllerNode::parametersCallback(const std::vector<rclcpp::Parameter> &parameters)
@@ -88,7 +101,7 @@ namespace controller
     {
       RCLCPP_INFO(this->get_logger(), "%s: %s", param.get_name().c_str(), param.value_to_string().c_str());
     }
-    new_params = true;
+    set_constants();
     return result;
   }
 
@@ -100,174 +113,60 @@ namespace controller
     twist_state = msg->current_vel.twist;
     twist_setpoint = msg->target_vel.twist;
 
-    if (using_joy)
-    {
-      if (!msg->buttons.empty())
-      {
-        active = msg->buttons[9];
-      }
-      else
-      {
-        active = false;
-      }
-    }
-    else
-    {
-      active = true;
-    }
+    // Compute current orientation (quat -> RPY)
+    double roll_state, pitch_state, yaw_state;
+    tf2::Quaternion q = tf2::Quaternion(msg->current_pos.pose.orientation.x, msg->current_pos.pose.orientation.y, msg->current_pos.pose.orientation.z, msg->current_pos.pose.orientation.w);
+    tf2::Matrix3x3 rot_matrix = tf2::Matrix3x3(q);
+    rot_matrix.getRPY(roll_state, pitch_state, yaw_state);
+    this->state_angle = tf2::Vector3(roll_state, pitch_state, yaw_state);
 
-    if (active)
-    {
-      double roll_state, pitch_state, yaw_state;
-      tf2::Quaternion q = tf2::Quaternion(msg->current_pos.pose.orientation.x, msg->current_pos.pose.orientation.y, msg->current_pos.pose.orientation.z, msg->current_pos.pose.orientation.w);
-      tf2::Matrix3x3 rot_matrix = tf2::Matrix3x3(q);
-      rot_matrix.getRPY(roll_state, pitch_state, yaw_state);
-      state_angle = tf2::Vector3(roll_state, pitch_state, yaw_state);
+    // Compute target orientation (quat -> RPY)
+    double roll_setpoint, pitch_setpoint, yaw_setpoint;
+    q = tf2::Quaternion(msg->target_pos.pose.orientation.x, msg->target_pos.pose.orientation.y, msg->target_pos.pose.orientation.z, msg->target_pos.pose.orientation.w);
+    rot_matrix = tf2::Matrix3x3(q);
+    rot_matrix.getRPY(roll_setpoint, pitch_setpoint, yaw_setpoint);
+    this->setpoint_angle = tf2::Vector3(roll_setpoint, pitch_setpoint, yaw_setpoint);
 
-      double roll_setpoint, pitch_setpoint, yaw_setpoint;
-      q = tf2::Quaternion(msg->target_pos.pose.orientation.x, msg->target_pos.pose.orientation.y, msg->target_pos.pose.orientation.z, msg->target_pos.pose.orientation.w);
-      // std::cout << msg->target_pos.pose.orientation.x << msg->target_pos.pose.orientation.y << msg->target_pos.pose.orientation.z << msg->target_pos.pose.orientation.w << std::endl;
-      rot_matrix = tf2::Matrix3x3(q);
-      rot_matrix.getRPY(roll_setpoint, pitch_setpoint, yaw_setpoint);
-
-      // Yaw hold
-      if (yaw_setpoint != 0)
-      {
-        yaw_hold = true;
-      }
-      else
-      {
-        if (abs(twist_setpoint.angular.z) <= 0.1 && yaw_hold == false)
-        {
-          yaw_setpoint = yaw_state;
-          yaw_hold_pos = yaw_state;
-          yaw_hold = true;
-        }
-        else if (yaw_hold && abs(twist_setpoint.angular.z) <= 0.1)
-        {
-          yaw_setpoint = yaw_hold_pos;
-        }
-        else
-        {
-          yaw_hold = false;
-        }
-      }
-      // Roll hold
-      if (abs(twist_setpoint.angular.x) <= 0.1 && roll_hold == false)
-      {
-        roll_setpoint = 0;
-        roll_hold_pos = 0;
-        roll_hold = true;
-      }
-      else if (roll_hold && abs(twist_setpoint.angular.x) <= 0.1)
-      {
-        roll_setpoint = roll_hold_pos;
-      }
-      else
-      {
-        roll_hold = false;
-      }
-      // Pitch holdstd::cout << "linear z position: " << pose_state.position.z << " EXPECTED: " << pose_setpoint.position.z << std::endl;
-      if (abs(twist_setpoint.angular.y) <= 0.1 && pitch_hold == false)
-      {
-        pitch_setpoint = pitch_state;
-        pitch_hold_pos = pitch_state;
-        pitch_hold = true;
-      }
-      else if (pitch_hold && abs(twist_setpoint.angular.y) <= 0.1)
-      {
-        pitch_setpoint = pitch_hold_pos;
-      }
-      else
-      {
-        pitch_hold = false;
-      }
-
-      setpoint_angle = tf2::Vector3(roll_setpoint, pitch_setpoint, yaw_setpoint);
-    }
+    active = (this->using_joy) ? (!msg->buttons.empty() && msg->buttons[9]): false;
   }
 
   void ControllerNode::publishState()
   {
-    if (active)
-    {
-      if (new_params)
-      {
-        set_constants();
-      }
+    geometry_msgs::msg::WrenchStamped controlEffort; // All zeroes
+    controlEffort.header.stamp = this->now();
+    controlEffort.header.frame_id = "base_link";
+    
+    if (active) {
       rclcpp::Time time = this->now(); // might have to change the now ????
-      if (lastTime.seconds() != 0)
-      {
-        double dt = (time - lastTime).nanoseconds();
-        if (dt != 0)
-        {
-          geometry_msgs::msg::WrenchStamped controlEffort;
-          controlEffort.header.stamp = this->now();
-          controlEffort.header.frame_id = "base_link";
-          controlEffort.wrench.force.x = -linear_x.computeCommand(twist_setpoint.linear.x - twist_state.linear.x, dt);
-          controlEffort.wrench.force.y = -linear_y.computeCommand(twist_setpoint.linear.y - twist_state.linear.y, dt);
-          controlEffort.wrench.force.z = linear_z.computeCommand(pose_setpoint.position.z - pose_state.position.z, dt);
-          if(this->get_parameter("debug").as_bool()){
-          std::cout << "\033[2J\033[1;1H" << std::endl;
-          std::cout << "linear x velocity: " << twist_state.linear.x << " EXPECTED: " << twist_setpoint.linear.x << std::endl;
-          std::cout << "linear y velocity: " << twist_state.linear.y << " EXPECTED: " << twist_setpoint.linear.y << std::endl; 
-          std::cout << "linear z position: " << pose_state.position.z << " EXPECTED: " << pose_setpoint.position.z << std::endl;
-          std::cout << "yaw (z) angle (pos): " << state_angle.getZ() << " EXPECTED (if yaw_hold = true): " << setpoint_angle.getZ() << std::endl;
-          std::cout << "yaw (z) twist (vel): " << twist_state.angular.z << " EXPECTED (if yaw_hold = false): " << twist_setpoint.angular.z << std::endl;
-          std::cout << "roll (x) angle (pos): " << state_angle.getX() << " EXPECTED (if roll_hold = true): " << setpoint_angle.getX() << std::endl;
-          std::cout << "roll (x) twist (vel): " << twist_state.angular.x << " EXPECTED (if roll_hold = false): " << twist_setpoint.angular.x << std::endl;
-          std::cout << "pitch (y) angle (pos): " << state_angle.getY() << " EXPECTED (if pitch_hold = true): " << setpoint_angle.getY() << std::endl;
-          std::cout << "pitch (y) twist (vel): " << twist_state.angular.y << " EXPECTED (if pitch_hold = false): " << twist_setpoint.angular.y << std::endl;
-          }
+      double dt = (time - lastTime).seconds();
 
-          if (yaw_hold)
-          {
-            // RCLCPP_INFO(this->get_logger(), "yaw hold");
-            controlEffort.wrench.torque.z = angular_z.computeCommand(angle_wrap_pi(setpoint_angle.getZ() - state_angle.getZ()), dt);
-          }
-          else
-          {
-            controlEffort.wrench.torque.z = angular_z.computeCommand(twist_setpoint.angular.z - twist_state.angular.z, dt);
-          }
-
-          if (roll_hold)
-          {
-            controlEffort.wrench.torque.x = angular_x.computeCommand(angle_wrap_pi(setpoint_angle.getX() - state_angle.getX()), dt);
-          }
-          else
-          {
-            controlEffort.wrench.torque.x = angular_x.computeCommand(twist_setpoint.angular.x - twist_state.angular.x, dt);
-          }
-
-          if (pitch_hold)
-          {
-            controlEffort.wrench.torque.y = angular_y.computeCommand(angle_wrap_pi(setpoint_angle.getY() - state_angle.getY()), dt);
-          }
-          else
-          {
-            controlEffort.wrench.torque.y = angular_y.computeCommand(twist_setpoint.angular.y - twist_state.angular.y, dt);
-          }
-
-          pubControlEffort->publish(controlEffort);
-        }
+      if (lastTime.seconds() == 0 || dt == 0) {
+        lastTime = time;
+        return;
       }
-      lastTime = time;
-    }
-    else
-    {
-      geometry_msgs::msg::WrenchStamped controlEffort;
-      controlEffort.header.stamp = this->now();
-      controlEffort.header.frame_id = "base_link";
-      controlEffort.wrench.force.x = 0;
-      controlEffort.wrench.force.y = 0;
-      controlEffort.wrench.force.z = 0;
 
-      controlEffort.wrench.torque.x = 0;
-      controlEffort.wrench.torque.y = 0;
-      controlEffort.wrench.torque.z = 0;
+      controlEffort.wrench.force.x = -linear_x.computeCommand(pose_state.position.x, pose_setpoint.position.x, twist_state.linear.x, twist_setpoint.linear.x, dt);
+      controlEffort.wrench.force.y = -linear_y.computeCommand(pose_state.position.y, pose_setpoint.position.y, twist_state.linear.y, twist_setpoint.linear.y, dt);
+      controlEffort.wrench.force.z = linear_z.computeCommand(pose_state.position.z, pose_setpoint.position.z, twist_state.linear.z, twist_setpoint.linear.z, dt);
 
-      pubControlEffort->publish(controlEffort);
+      controlEffort.wrench.torque.x = angular_x.computeCommand(state_angle.getX(), setpoint_angle.getX(), twist_state.angular.x, twist_setpoint.angular.x, dt);
+      controlEffort.wrench.torque.y = angular_y.computeCommand(state_angle.getY(), setpoint_angle.getY(), twist_state.angular.y, twist_setpoint.angular.y, dt);
+      controlEffort.wrench.torque.z = angular_z.computeCommand(state_angle.getZ(), setpoint_angle.getZ(), twist_state.angular.z, twist_setpoint.angular.z, dt);
+
+      if(this->get_parameter("debug").as_bool()) {
+        std::cout << "\033[2J\033[1;1H" << std::endl;
+        std::cout << "linear x velocity: " << twist_state.linear.x << " EXPECTED: " << twist_setpoint.linear.x << std::endl;
+        std::cout << "linear y velocity: " << twist_state.linear.y << " EXPECTED: " << twist_setpoint.linear.y << std::endl; 
+        std::cout << "linear z position: " << pose_state.position.z << " EXPECTED: " << pose_setpoint.position.z << std::endl;
+        std::cout << "yaw (z) angle (pos): " << state_angle.getZ() << " EXPECTED (if yaw_hold = true): " << setpoint_angle.getZ() << std::endl;
+        std::cout << "yaw (z) twist (vel): " << twist_state.angular.z << " EXPECTED (if yaw_hold = false): " << twist_setpoint.angular.z << std::endl;
+        std::cout << "roll (x) angle (pos): " << state_angle.getX() << " EXPECTED (if roll_hold = true): " << setpoint_angle.getX() << std::endl;
+        std::cout << "roll (x) twist (vel): " << twist_state.angular.x << " EXPECTED (if roll_hold = false): " << twist_setpoint.angular.x << std::endl;
+        std::cout << "pitch (y) angle (pos): " << state_angle.getY() << " EXPECTED (if pitch_hold = true): " << setpoint_angle.getY() << std::endl;
+        std::cout << "pitch (y) twist (vel): " << twist_state.angular.y << " EXPECTED (if pitch_hold = false): " << twist_setpoint.angular.y << std::endl;
+      }
     }
+    pubControlEffort->publish(controlEffort);
   }
 }
 
